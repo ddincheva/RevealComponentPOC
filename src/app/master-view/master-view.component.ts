00b5514080474c577e5265b3b40e92c7ebc114e3
ddincheva <desislava9dincheva@gmail.com>
2023-04-27 09:22:26 +0300

    chore(*): basic reveal dashboard app

--- src/app/master-view/master-view.component.ts
+++ src/app/master-view/master-view.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { NorthwindService } from '../services/northwind.service';
 declare let $: any;
 
@@ -8,14 +8,7 @@ declare let $: any;
   styleUrls: ['./master-view.component.scss']
 })
 export class MasterViewComponent implements AfterViewInit {
-  readonly EMPTY_IMAGE_PATH = '';
-  public visible = false;
-
-  @Input()
-  public src = 'about:blank';
-
   public northwindEmployees: any = null;
-  public financialBoxOfficeRevenue: any = null;
 
   @ViewChild('revealView') el!: ElementRef;
 
@@ -28,20 +21,12 @@ export class MasterViewComponent implements AfterViewInit {
     this.northwindService.getData('Employees').subscribe(data => this.northwindEmployees = data);
   }
   ngAfterViewInit(): void {
-     $("div").click(function(){
-      console.log('there is JQuery');
-    });
       $.ig.RevealSdkSettings.setBaseUrl("https://samples.revealbi.io/upmedia-backend/reveal-api/");
 
       $.ig.RVDashboard.loadDashboard("Sales", (dashboard: any) => {
-          console.log(this.el.nativeElement);
           var revealView = new $.ig.RevealView(this.el.nativeElement);
           revealView.dashboard = dashboard;
       });
     }
-
-  changeVisibility() {
-    this.visible = !this.visible;
-  }
 
 }
